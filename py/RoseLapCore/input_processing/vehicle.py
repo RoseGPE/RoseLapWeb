5ee36ee02140d46e1a31039bafc758544b57b066
Thaddeus Hughes <hughes.thad@gmail.com>
2018-06-06 22:48:44 -0400

    Cleaning up vehicle documentation

--- py/RoseLapCore/input_processing/vehicle.py
+++ py/RoseLapCore/input_processing/vehicle.py
@@ -18,22 +18,27 @@ class Vehicle(object):
     return 1 - self.brake_bias
 
   def eng_force(self, vel, gear):
+    # Compute the angular speed of the crankshaft. 9.5493 is a conversion factor from rad/s to RPM.
     gear_ratio = self.gears[gear]
     eng_output_rpm = vel / self.tire_radius * 9.5493 * self.final_drive_reduction
     crank_rpm = eng_output_rpm * self.engine_reduction * gear_ratio
 
     if crank_rpm <= self.engine_rpms[0]:
-      # cap to lowest hp
+      # If under RPM range, use the lowest torque value
       return (self.engine_torque[0] * self.engine_reduction * gear_ratio * self.final_drive_reduction / self.tire_radius, crank_rpm)
     elif crank_rpm > self.engine_rpms[-1]:
-      return (0,crank_rpm) # simulate hitting the rev limiter
+      # If over RPM range, no power because you're hitting the rev limiter
+      return (0,crank_rpm)
     else:
+      # Otherwise, linearly interpolate
       for i in range(1, len(self.engine_rpms)):
         if crank_rpm < self.engine_rpms[i]:
           torque = self.engine_torque[i] + (crank_rpm - self.engine_rpms[i]) * (self.engine_torque[i-1] - self.engine_torque[i]) / (self.engine_rpms[i-1] - self.engine_rpms[i])
           return (torque * self.engine_reduction * gear_ratio * self.final_drive_reduction / self.tire_radius, crank_rpm)
 
   def best_gear(self, v, fr_limit):
+    # Find the best gear and return the number for it.
+    # Doesn't actually do anything with the friction limitation. It did at one point.
     opts = [self.eng_force(v, int(gear))[0] for gear in range(len(self.gears))]
     best = 0
     besti = -1
@@ -53,18 +58,13 @@ class Vehicle(object):
     return (f_lat*n_tires, f_y_max*n_tires)
 
   def f_long_remain(self, n_tires, f_norm, f_lat):
-    # Force that one of the tires in this set produces
     f_x_max = self.tire_mu_x*(f_norm/n_tires) + self.tire_offset_x
     f_y_max = self.tire_mu_y*(f_norm/n_tires) + self.tire_offset_y
 
     if f_y_max < abs(f_lat/n_tires):
       return (-1, f_x_max*n_tires)
     f_long = math.sqrt(max(1-(abs(f_lat)/n_tires)**2/f_y_max/f_y_max,0))*f_x_max
-    # print('%f, %f, %f, %f, %f' % (f_norm, f_lat, f_x_max, f_y_max, f_long))
     return (f_long*n_tires, f_x_max*n_tires)
-    # if f_norm*self.tire_mu_x < abs(f_lat):
-    #   return (-1, f_norm*self.tire_mu_x)
-    # return (math.sqrt((f_norm*self.tire_mu_x)**2 - f_lat**2), f_norm*self.tire_mu_x)
 
   def prep(self):
     self.mass = self.mass / self.g
@@ -72,12 +72,6 @@ class Vehicle(object):
   def __init__(self, v_OBJ):
     self.g = g
     self.__dict__.update(v_OBJ.__dict__)
-
-  # def __setattr__(self, name, value):
-  #   if name == "mass":
-  #     self.__dict__[name] = value / self.g
-  #   else:
-  #     self.__dict__[name] = value
 
 def v_load(filename):
   vehicle_YAML = './params/vehicles/' + filename
@@ -87,8 +81,3 @@ def v_load(filename):
 
 def v_getOriginalVal(name):
   return v.v_OBJ[name]
-
-# def v_setVar(name, val):
-#   if name == 'mass':
-#     val /= g
-#   exec("v." + name + " = " + str(val)) # jank in preparation for even better code structure
