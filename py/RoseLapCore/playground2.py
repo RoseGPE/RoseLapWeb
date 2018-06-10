bf8c7c043185119f13f42a95abf78871329e3853
Thaddeus Hughes <hughest1@rose-hulman.edu>
2018-06-09 22:47:50 -0400

    Understand the parser now. It's fine. 1 px = 1 base unit (i.e. ft)

--- py/RoseLapCore/playground2.py
+++ py/RoseLapCore/playground2.py
@@ -4,27 +4,40 @@ import input_processing.fancyyaml as yaml
 import input_processing.track_segmentation as trackseg
 import plottools
 
-sim = sims.Simulation("point_mass");
+from svgpathtools import *
+import numpy as np
+import matplotlib.pyplot as plt
 
-vehicle  = vehicle.Vehicle(yaml.load(open('params/vehicles/rgp008_baseline.yaml','r'),True))
-vehicle.prep()
-segments = trackseg.file_to_segments('params/DXFs/test.svg', 1)
+# sim = sims.Simulation("point_mass");
 
-plottools.plot_velocity_and_events(sim.solve(vehicle, segments[0:int(len(segments)*5/5)]),'t','Vehicle 1')
+# vehicle  = vehicle.Vehicle(yaml.load(open('params/vehicles/rgp008_baseline.yaml','r'),True))
+# vehicle.prep()
+# segments = trackseg.file_to_segments('params/DXFs/test.svg', 1)
 
-plottools.plt.show();
-# def points_in_each_seg_slow(path, dx):
-#     a = np.array([])
-#     for seg in path:
-#         a = np.append(a, seg.poly()(np.linspace(0,1,seg.length()/dx)))
-#     return np.stack((a.real,a.imag),axis=1)
+# plottools.plot_velocity_and_events(sim.solve(vehicle, segments[0:int(len(segments)*5/5)]),'t','Vehicle 1')
 
-# if __name__ == '__main__':
-#     testpath,attrs = svg2paths('params/DXFs/test.svg') 
-#     testpath = testpath[0]
-#     tvals = np.linspace(0, 1, 10)
+# plottools.plt.show();
 
-#     pts = points_in_each_seg_slow(testpath, 4)
-#     plt.plot(pts[:,0],pts[:,1],'.')
 
-#     plt.show()
+
+
+
+def points_in_each_seg_slow(path, dx):
+    a = np.array([])
+    for seg in path:
+        a = np.append(a, seg.poly()(np.linspace(0,1,seg.length()/dx)))
+    return np.stack((a.real,a.imag),axis=1)
+
+if __name__ == '__main__':
+    testpath,attrs = svg2paths('params/DXFs/test.svg') 
+    testpath = testpath[0]
+    tvals = np.linspace(0, 1, 10)
+
+    print(testpath)
+
+    pts = points_in_each_seg_slow(testpath, 4)
+    pts = pts-pts[0,:]
+    print(pts)
+    plt.plot(pts[:,0],pts[:,1],'.')
+
+    plt.show()
