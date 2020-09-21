16ed977321e6b5eb7233b01be91822de16352513
Thaddeus Hughes <hughes.thad@gmail.com>
2020-09-21 16:06:36 -0400

    Create channel_names.py

---
+++ channel_names.py
@@ -0,0 +1,33 @@
+# Remember to follow ISO coordinate system (x/y/z -> fwd/left/up -> roll/pitch/yaw)
+
+DIST - Distance
+CURV - Curvature
+
+ACCX - X Acceleration
+ACCY - Y Acceleration
+ACCZ - Z Acceleration
+VELX - X Velocity
+VELY - Y Velocity
+VELZ - Z Velocity
+
+ALPX - RX Acceleration
+ALPY - RY Acceleration
+ALPZ - RZ Acceleration
+OMGX - RX Velocity
+OMGY - RY Velocity
+OMGZ - RZ Velocity
+
+GEAR - Gear
+TPCT - Throttle Percentage
+ERPM - Engine RPM
+PCNS - Power Consumption
+PEFF - Power Efficiency
+
+FT1X - Force on tire 1, X
+.... - All tire forces follow same precedent
+
+FAEX - X Aero Force
+FAEY - Y Aero Force
+FAEZ - Z Aero Force
+FMOD - Aero Mode
+
